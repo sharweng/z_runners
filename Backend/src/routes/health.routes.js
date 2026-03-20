24bd18e325150bf3588bffd361887cccf5f5ecfa
sharweng <162144450+sharweng@users.noreply.github.com>
2026-03-20 19:56:12 +0800

    another backend

--- Backend/src/routes/health.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getHealth } = require("../controllers/health.controller");
-
-const router = express.Router();
-
-router.get("/health", getHealth);
-
-module.exports = router;
